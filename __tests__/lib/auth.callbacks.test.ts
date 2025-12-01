a7c17898154bcb636b97324d5e51c8e88167b733
Joel Klabo <joelklabo@gmail.com>
2025-11-30 21:09:07 -0800

    Satisfy AdapterUser types in session test

--- __tests__/lib/auth.callbacks.test.ts
+++ __tests__/lib/auth.callbacks.test.ts
@@ -77,6 +77,7 @@ describe("auth callbacks", () => {
           name: "Me",
           image: undefined,
           username: undefined,
+          emailVerified: null,
         },
         expires: "",
       },
