0a2a8b012458df870d6ea1b0837b76a48dc05bce
Joel Klabo <joelklabo@gmail.com>
2025-12-02 10:58:19 -0800

    test: polyfill ResizeObserver for tooltips

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,12 @@
 /// <reference types="vitest" />
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Radix Tooltip and Drawer use ResizeObserver; jsdom doesn't provide it by default.
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+const globalWithRO = globalThis as typeof globalThis & { ResizeObserver?: typeof ResizeObserver };
+globalWithRO.ResizeObserver = ResizeObserver;
