b4fc9d87e686c6b9ec65ea28ca346ad81599cda8
Joel Klabo <joelklabo@gmail.com>
2025-12-18 14:23:38 -0800

    bd markdowntown-np6j.15: CityLogoLab playground

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,17 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const storybookDir = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx|mdx)", "../src/stories/**/*.stories.@(ts|tsx|mdx)"],
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-toolbars",
+    "@storybook/addon-backgrounds",
+    "@storybook/addon-actions",
+    "@storybook/addon-controls",
+    "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-links",
   ],
@@ -16,6 +23,14 @@ const config: StorybookConfig = {
     autodocs: true,
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.resolve ??= {};
+    config.resolve.alias ??= {};
+    if (typeof config.resolve.alias === "object" && config.resolve.alias) {
+      config.resolve.alias["@"] = path.resolve(storybookDir, "../src");
+    }
+    return config;
+  },
 };
 
 export default config;
